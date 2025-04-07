67201ed42ee0a57105a0e8993b520c030b0a9f0d
Aaron Soliz <aaronmsoliz@hotmail.com>
2025-04-07 13:01:40 -0700

    Menus, images, and featured items display added

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,89 @@
+//variables for menu items
+const listDisplay = document.querySelector(".menu");
+const menuDisplay = document.querySelector(".menu-items");
 //variables for review slider
 const slides = document.querySelectorAll(".slides p");
 let slideIndex = 0;
 //variables for FAQ accordion
 let question = document.getElementsByClassName("faq-question");
 
-/* review slider */
-//show first slide on startup
+//render menu and review slider on startup
+document.addEventListener("DOMContentLoaded", menuJsonToVariable);
 document.addEventListener("DOMContentLoaded", initializeSlider);
+
+/*menu items*/
+//fetch data from json file
+async function getJsonData(){
+    try{
+        //fetch data from json file
+        const response = await fetch('./json/limitedMenu.json');
+        if(!response.ok){
+            throw new Error(`Status: ${response.status}`);
+        }
+        //convert response into readable json
+        const jsonData = await response.json();
+        return jsonData;
+    } catch(error) {
+        console.error("Could not fetch the data:", error);
+        return null;
+    }
+}
+//render menu items to page
+async function menuJsonToVariable(){
+    const menuData = await getJsonData();
+    if(menuData){
+        console.log("Menu:", menuData);
+        //iterate through menu by each section of json
+        Object.keys(menuData).forEach(key => {
+            menuDisplay.classList.add("menuDisplay");
+            //create title element
+            const sectionDisplay = document.createElement("h2");
+            sectionDisplay.textContent = key;
+            sectionDisplay.classList.add("sectionDisplay");
+            menuDisplay.appendChild(sectionDisplay);
+            //create item container element
+            const listDisplay = document.createElement("div");
+            listDisplay.classList.add("listDisplay");
+            menuDisplay.appendChild(listDisplay);
+            //iterate for loop for amount of items
+            const sectionLength = Object.keys(menuData[key]).length;
+            console.log(`sectionLength: ${sectionLength}`);
+            for(let i = 0; i < sectionLength; i++){
+                //make new menu item
+                const itemDisplay = document.createElement("div");
+                //make sections for name, image, price, calories, and description
+                const nameDisplay = document.createElement("p");
+                const imgDisplay = document.createElement("img");
+                //test price and cal in one element
+                const statDisplay = document.createElement("p");
+                const descDisplay = document.createElement("p");
+                //text content
+                nameDisplay.textContent = menuData[key][i].name;
+                imgDisplay.src = menuData[key][i].img;
+                statDisplay.textContent = `$${menuData[key][i].price} | ${menuData[key][i].cal} cal`;
+                descDisplay.textContent = menuData[key][i].desc;
+                //add class for css
+                itemDisplay.classList.add("itemDisplay");
+                nameDisplay.classList.add("nameDisplay");
+                imgDisplay.classList.add("imgDisplay");
+                statDisplay.classList.add("statDisplay");
+                descDisplay.classList.add("descDisplay");
+                //append displays to item and menu displays
+                itemDisplay.appendChild(nameDisplay);
+                itemDisplay.appendChild(imgDisplay);
+                itemDisplay.appendChild(statDisplay);
+                itemDisplay.appendChild(descDisplay);
+                
+                listDisplay.appendChild(itemDisplay);
+            }
+        });        
+    } else{
+        console.log("Failed to load JSON file.");
+    }
+}
+
+/* review slider */
+//show first slide
 function initializeSlider(){
     if(slides.length > 0){
         slides[slideIndex].classList.add("displaySlide");
